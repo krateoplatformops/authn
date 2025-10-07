dcee29facadbae9335726aa67fb4cd148953e33f
FrancescoL96 <francesco29.fl@gmail.com>
2025-10-07 10:33:46 +0200

    fix: improved fallback detection

--- internal/routes/auth/oauth/login.go
+++ internal/routes/auth/oauth/login.go
@@ -109,7 +109,9 @@ func (r *loginRoute) Handler() http.HandlerFunc {
 				return
 			}
 			additionalFieldstoReplace, err := restaction.Resolve(r.ctx, r.rc, restactionRef, uuid.New().String(), tok.AccessToken)
-			if err != nil {
+			value, ok := additionalFieldstoReplace["name"]
+			_, okk := additionalFieldstoReplace["name"].(string)
+			if err != nil || !ok || !okk || value == nil {
 				log.Err(err).Str("name", name).Msg("unable to resolve restaction, retrying with legacy resolve (copy)")
 				additionalFieldstoReplace, err = restaction.LegacyResolve(r.ctx, r.rc, restactionRef, uuid.New().String(), tok.AccessToken)
 				if err != nil {
--- internal/routes/auth/oidc/login.go
+++ internal/routes/auth/oidc/login.go
@@ -94,9 +94,11 @@ func (r *loginRoute) Handler() http.HandlerFunc {
 		log.Debug().Str("name", name).Msg("resolving restaction")
 		if cfg.RESTActionRef != nil {
 			additionalFieldstoReplace, err := restaction.Resolve(r.ctx, r.rc, cfg.RESTActionRef, idToken.email, idToken.bearerToken)
-			if err != nil {
-				additionalFieldstoReplace, err = restaction.Resolve(r.ctx, r.rc, cfg.RESTActionRef, idToken.email, idToken.bearerToken)
+			value, ok := additionalFieldstoReplace["name"]
+			_, okk := additionalFieldstoReplace["name"].(string)
+			if err != nil || !ok || !okk || value == nil {
 				log.Err(err).Str("name", name).Msg("unable to resolve restaction, retrying with legacy resolve (copy)")
+				additionalFieldstoReplace, err = restaction.Resolve(r.ctx, r.rc, cfg.RESTActionRef, idToken.email, idToken.bearerToken)
 				if err != nil {
 					log.Err(err).Str("name", name).Msg("unable to resolve restaction, stopping")
 					encode.InternalError(wri, err)
